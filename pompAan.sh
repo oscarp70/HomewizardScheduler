c736011fa5e18c0d95da392c94d1fdf16a7c0092
oscarp70 <o.pieters@gmail.com>
2020-04-14 19:42:33 +0200

    First full release

--- pompAan.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/curl http://192.168.178.100:85/geheim/sw/9/on
-sleep 5
-/usr/bin/curl http://192.168.178.100:85/geheim/sw/9/on
--- req.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-r =requests.get('http://192.168.178.100:85/geheim/sw/2/off')
